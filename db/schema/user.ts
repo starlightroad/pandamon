471696baa8cafe9448b97486890361195b196190
starlightroad <ortizge92@gmail.com>
2025-10-10 22:09:02 -0400

    Build the schemas and configure the Drizzle config

---
+++ db/schema/user.ts
@@ -0,0 +1,14 @@
+import { boolean, pgTable, text, timestamp } from "drizzle-orm/pg-core";
+
+export const usersTable = pgTable("user", {
+  id: text("id").primaryKey(),
+  name: text("name").notNull(),
+  email: text("email").notNull().unique(),
+  emailVerified: boolean("email_verified").default(false).notNull(),
+  image: text("image"),
+  createdAt: timestamp("created_at", { mode: "string" }).defaultNow().notNull(),
+  updatedAt: timestamp("updated_at", { mode: "string" })
+    .defaultNow()
+    .$onUpdate(() => new Date().toISOString())
+    .notNull(),
+});
