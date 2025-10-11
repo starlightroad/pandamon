f425e410cbf2ece2dcea8198b76a6833722ed555
starlightroad <ortizge92@gmail.com>
2025-10-11 17:58:39 -0400

    Allow users to view password before creating an account

--- app/_features/authentication/components/sign-up-form.tsx
+++ app/_features/authentication/components/sign-up-form.tsx
@@ -4,9 +4,14 @@ import { useState } from "react";
 
 import { useFormState } from "react-dom";
 
-import { Flex, Text, TextField } from "@radix-ui/themes";
+import { Flex, IconButton, Text, TextField } from "@radix-ui/themes";
 
-import { CircleUserRoundIcon, KeyRoundIcon } from "lucide-react";
+import {
+  CircleUserRoundIcon,
+  EyeIcon,
+  EyeOffIcon,
+  KeyRoundIcon,
+} from "lucide-react";
 
 import formStyles from "@/public/styles/form.module.css";
 
@@ -21,6 +26,7 @@ export default function SignUpForm() {
 
   const [emailValue, setEmailValue] = useState("");
   const [passwordValue, setPasswordValue] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
 
   return (
     <Flex direction="column" gap="3" asChild>
@@ -52,7 +58,7 @@ export default function SignUpForm() {
           </Text>
           <TextField.Root
             id="password"
-            type="password"
+            type={showPassword ? "text" : "password"}
             name="password"
             placeholder="Password"
             size="3"
@@ -63,6 +69,21 @@ export default function SignUpForm() {
           >
             <TextField.Slot>
               <KeyRoundIcon size={15} />
+            </TextField.Slot>
+            <TextField.Slot>
+              <IconButton
+                type="button"
+                variant="ghost"
+                color="gray"
+                size="1"
+                onClick={() => setShowPassword((prevState) => !prevState)}
+              >
+                {showPassword ? (
+                  <EyeIcon size={15} />
+                ) : (
+                  <EyeOffIcon size={15} />
+                )}
+              </IconButton>
             </TextField.Slot>
           </TextField.Root>
         </Flex>
